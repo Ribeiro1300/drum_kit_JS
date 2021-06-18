097adb42b8be3afc9c52146047d404300d13b847
Ribeiro1300 <59618809+Ribeiro1300@users.noreply.github.com>
2021-06-17 22:26:20 -0300

    primeira versão

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var n_buttons = document.querySelector(".drum").length
+for (var i = 0; i<n_buttons; i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+
+
+    })
+}
